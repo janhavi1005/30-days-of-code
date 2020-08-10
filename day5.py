ad4422799ec5e310d14a2b62abbc82227e61fa8d
janhavi1005 <69475676+janhavi1005@users.noreply.github.com>
2020-08-10 13:06:33 -0400

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if _name_ == '_main_':
+    n = int(input())
+for i in range(1,11):
+    print(n, "x", i, "=", n*i)
